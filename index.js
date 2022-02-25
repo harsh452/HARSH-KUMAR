8347eeb880fbd72c0910c187d7a6e123c4f755c0
Harsh <70992725+harsh452@users.noreply.github.com>
2022-02-25 13:30:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const { default: axios } = require('axios');
 const express = require('express')
 const app = express()
 var request = require('request');
-
+app.use(express.json());
 var port = process.env.PORT || 5000;
 
 var letters = new Set();
 
-
-
-app.get("/:servername/:dataType", (req, res,next) => {
+app.get("/:servername/:dataType", (req, res) => {
 let servername = req.params.servername;
 let dataType = req.params.dataType
 axios.get(` http://localhost:${servername}/${dataType}`).then((response)=>{
@@ -17,10 +15,19 @@ axios.get(` http://localhost:${servername}/${dataType}`).then((response)=>{
   for(var i in response.data.numbers)
   letters.add( response.data.numbers[i]);
 
- console.log(letters);
+
+
 }).catch((err)=>{
   
 })
+var sortedNumbers = Array.from(letters).sort((a, b) => a - b);
+var sortedLetters = new Set(sortedNumbers);
+res.send({
+    sortedLetters
+})
+console.log(sortedLetters);
+
+
 });
 
 app.get("/",(req,res)=>{
@@ -33,5 +40,6 @@ app.get("/",(req,res)=>{
 app.listen(port,() =>{
     console.log("listening on 5000");
 })
+
 // Not able to display the json data at the moment but the program is able to fetch the data and sort it;
 
